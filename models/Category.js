5d4ad0afff90e7ed6c8dd44eff3de5040edfada7
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-01 15:00:59 -0700

    working on the models

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -18,3 +27,4 @@ Category.init(
 );
 
 module.exports = Category;
+
