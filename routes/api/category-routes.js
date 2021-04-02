8d7d7a3e6f6fb4a198ebc39b030f0fd32805f892
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-02 08:39:34 -0700

    working on product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,18 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-//localhost:3001/api/category
+//localhost:3001/api/categories
 
 router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }],
+      include: { model: Product, attributes: ["product_name"], },
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err.message);
+    res.status(500).json(err);
   }
 });
 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,7 +41,7 @@ router.post("/", async (req, res) => {
     const produtData = await Product.create({
       product_name: req.body.product_name,
       price: req.body.price,
-      stock: req.body.price,
+      stock: req.body.stock,
     });
     res.status(200).json(produtData);
   } catch (err) {
@@ -114,7 +114,6 @@ router.put("/:id", (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
